908cad17a7595a130c6542331960dbc60d2aaf2c
ganeshnikam29@gmail.com <ganeshnikam29@gmail.com>
2021-05-22 13:17:25 +0530

    initial commit

---
+++ server/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+  redisHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT,
+  pgHost:process.env.POSTGRES_HOST,
+  pgUser: process.env.POSTGRES_USER,
+  pgDatabase: process.env.POSTGRES_DB,
+  pgPassword: process.env.POSTGRES_PASSWORD,
+  pgPort: process.env.POSTGRES_PORT
+};
